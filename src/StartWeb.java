2ce803011361fdfd83bd305486059455fbc5d7ca
taewan625 <taewan1997625@gmail.com>
2024-08-03 18:04:33 +0900

    Refactor : MessageSource.java
    메시지 프로퍼티를 읽는 Class 리팩토링 수행
    1. 동적으로 properties가 추가 삭제되는 여부와 관계 없이 동작
    2. 자원 해제하여 메모리 누수 방지
    3. 원복 객체를 직접적으로 변경하지 못하도록 접근 제어자 변경 및 조회용 메서드 생성
    
    MessageSource.java의 TODO는 차후에 진행 후 제거 예정

--- src/StartWeb.java
+++ src/StartWeb.java
@@ -1,5 +1,4 @@
 import java.util.Map;
-import java.util.Properties;
 
 import com.config.SpringContainer;
 import com.propertiesconvert.MessageSource;
@@ -13,7 +12,7 @@ import sta.sta1000.sta1000.service.impl.Sta1000DAO;
 import usr.usr1000.usr1000.service.impl.Usr1000DAO;
 
 /**
- * @Class Name : HealthProgrem.java
+ * @Class StartWeb : StartWeb.java
  * @Description : 헬스프로그램을 시작하는 서버 클래스
  * 
  * @version 1.0
@@ -23,8 +22,6 @@ import usr.usr1000.usr1000.service.impl.Usr1000DAO;
  * @see Copyright (C) All right reserved.
  */
 public class StartWeb {
-	//TODO MessageSource를 가지고 오는 파일이 없어서 에러 발생  이부분부터 수정 필요 
-	public static Properties message = MessageSource.properties;
 
 	/**
 	 * Func : 헬스프로그램 서버 실행 및 데이터 초기화
@@ -33,34 +30,45 @@ public class StartWeb {
 	 * 
 	 * @param String[] args
 	 * @return void
-	 * @throws Exception,
+	 * @throws Exception
 	 */
 	public static void main(String[] args) {
 		/*
 		 * TODO 1. StartWeb에 설정파일 초기화 수행 2. Config.java 파일을 읽어서 BeanContainer에
 		 * handler-Service-DAO 인스턴스 등록 3. DB 등록 4. DB와 DAO 연결
+		 * 
+		 * TODO DB가 실제 웹 컨테이너와 어떤 식으로 연결이 되는지 파악이 필요로 함.
+		 * 콘솔(사용자) - startWeb(서버 구동) - 실제 Service(Controller-Service-DAO) 구성 - DB (java파일로 영역 분리)
+		 * 
+		 * TODO 이번 목적은 Java, MVC 구조 적용이다. -Spring 설정 순서 혹은 web service 올릴 때 올라가는 파일 순서는 고려하지 말 것
+		 * 최소한으로 모든 것을 끝낸다.
 		 */
-		// webServlet Container에 dispatcherServlet 등록
 		try {
-			// db 등록
+			//db 등록
 			USR1000 userDB = USR1000.getInstance();
 			LCK1000 lockerDB = LCK1000.getInstance();
-			// springContainer 등록
+			
+			//springContainer 등록
 			SpringContainer springContainer = SpringContainer.getInstance();
 			Map<String, Object> beanContainer = springContainer.beanContainer;
-			// db 연결
+
+			//db 연결
 			connectDB(userDB, lockerDB, beanContainer);
-			// Welcome page 요청 생성
-			Request request = new Request(message.getProperty("menu"));
+			
+			//Welcome page 요청 생성
+			Request request = new Request(MessageSource.getMessage("message.menu"));
+			
 			// weclome page 요청처리 수행
 			DispatcherServlet dispatcherServlet = DispatcherServlet.getInstance();
 			dispatcherServlet.service(request);
+			
 		} catch (Exception e) {
+			//오류페이지 역할
 			System.out.println("웹 서버 초기화 시, 문제가 발생했습니다. 다시 시도하십시오");
-			Request request = new Request(message.getProperty("err"));
 		}
 	}
 
+	
 	private static void connectDB(USR1000 userDB, LCK1000 lockerDB, Map<String, Object> beanContainer)
 			throws Exception {
 		try {
--- src/com/propertiesconvert/MessageSource.java
+++ src/com/propertiesconvert/MessageSource.java
@@ -1,8 +1,12 @@
 package com.propertiesconvert;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+
 
 /**
  * @Class Name : MessageSource.java
@@ -13,17 +17,65 @@ import java.util.Properties;
  * @Modification Information
  * @see Copyright (C) All right reserved.
  */
+
 public class MessageSource {
-	public static final Properties properties = new Properties();
-
-	// static 초기화 메서드
+	//메시지 프로퍼티 객체 생성 
+	private static final Map<String, Properties> properties = new HashMap<>();
+	
+	//class loading 시점에 초기화
 	static {
+		//.properties가 존재하는 경로
+		String propertiesPath = "/Users/taewan/Documents/develop/project/health-club/health-club-java/src/resource";		
+		
+		//경로 내 탐색한 파일명 조회
+		String[] filenames = new File(propertiesPath).list();
+		
+		//파일 경로마다 프로퍼티 추가
+        for (String filename : filenames) {
+        	//.properties 파일인 경우만 map에 담기 
+        	if (filename.contains(".properties")) {
+        		//try-with-resource로 자원 누수 방지
+                try (FileInputStream fis = new FileInputStream(propertiesPath + "/" + filename)) {
+                    //프로퍼티 객체 생성
+                	Properties prop = new Properties();
+                	//파일 정보 담기
+                    prop.load(fis);
+                    //map에 담기
+                    properties.put(filename.split("\\.")[0], prop);
+                    
+                } catch (IOException e) {
+        			//*1.URL 경로도 해당 프로퍼티에 존재하기 때문에 예외 던지기로 프로그램 강제 종료
+        			//TODO readMe에 들어갈 내용 - 내부클래스는 클래스 로더 시점이 달라서 try-catch롤 잡을 수 없어서 이 시점에 예외를 발생 시킨 후 강제종료 
+        			throw new RuntimeException(e);
+        		}
+        	}
+        }
+        
+        //더 이상 사 안하는 객체 참조 해제
+        filenames = null;
+	}
+	
+	/**
+	 * @desc   properties 객체에 직접 접근하지 않고 값만 반환
+	 * @param  key
+	 * @return String
+	 */
+	public static String getMessage(String key) {
+		//반환할 변수
+		String value = "";
+		
 		try {
-			properties.load(new FileInputStream(
-					"/Users/taewan/Documents/develop/project/health-club/health-club-java/src/resource/message.properties"));
-		} catch (IOException e) {
-			System.out.println("properties에서 문제가 발생했습니다.");
+			//split key and file name
+			String[] split = key.split("\\.");
+			//get value
+			value = properties.get(split[0]).getProperty(split[1]);
+			
 		}
-
+		//올바르지 않는 key 접근으로 예외 발생 시, 오류 로그 표출 및 빈 문자열 반환
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return value;
 	}
 }
