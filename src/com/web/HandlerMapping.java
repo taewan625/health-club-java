7a1d9bab86b921762286a889afd7aa4f32e7fb6b
taewan625 <taewan1997625@gmail.com>
2024-08-06 22:53:09 +0900

    Refactor : Fix HandlerMapping 매개변수, 변수명, 타입, 주석 수정

--- src/com/web/HandlerMapping.java
+++ src/com/web/HandlerMapping.java
@@ -16,45 +16,47 @@ import com.MessageSource;
  * @see Copyright (C) All right reserved.
  */
 public class HandlerMapping {
-	//실제 Class 객체와 method 객체가 담긴 객체를 반환 
-	public Map<String, Object> getHandler(Request request) {
+	/**
+	 * @desc   실제 Class 객체와 method 객체가 담긴 객체를 반환 
+	 * @param  Request request
+	 * @return Map<String, Object>
+	 */
+	public static Map<String, Object> getHandler(Request request) {
 		//반환 객체
 		Map<String, Object> returnMap = new HashMap<>();
-
-		//URL 요청과 관련된 Controller 정보 호출
-		String rawHandlerInfo = MessageSource.getMessage("bean." + request.getUrl());
-
-		//URL이 없는 경우 - Error Page Controller 정보 호출
-		if (null == rawHandlerInfo) {
-			rawHandlerInfo = MessageSource.getMessage("bean.errorPage");
-		}
 		
-		//controller 정보와 method 정보 분리
-		String[] handlerInfo = rawHandlerInfo.split("-");
-		
-		//Reflection API를 이용한 class 및 method 객체 생성
 		try {
-			//클래스
+			//URL 요청과 관련된 Controller 정보 호출
+			String rawHandlerInfo = MessageSource.getMessage("bean." + request.getUrl());
+	
+			//URL이 없는 경우 - Error Page Controller 정보 호출
+			if (null == rawHandlerInfo) {
+				rawHandlerInfo = MessageSource.getMessage("bean.errorPage");
+			}
+			
+			//controller 정보와 method 정보 분리
+			String[] handlerInfo = rawHandlerInfo.split("-");
+		
+			//클래스 객체 생성
 			Class<?> targetClass = Class.forName(handlerInfo[0]);
 			
 			//싱글톤 호출 메서드
 			Method getInstance = targetClass.getMethod("getInstance");
 			
 			//클래스 인스턴스
-			Object targetInstance = getInstance.invoke(targetClass);
+			Object classInstance = getInstance.invoke(targetClass);
 			
 			//메서드
-			Method method = targetClass.getMethod(handlerInfo[1], Request.class);
+			Method method = targetClass.getMethod(handlerInfo[1], Request.class, Response.class);
 			
 			//반환 객체에 데이터를 담는다.
-			returnMap.put("targetInstance", targetInstance);
+			returnMap.put("classInstance", classInstance);
 			returnMap.put("method", method);
 			
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 		
 		return returnMap;
 	}
-	
 }
