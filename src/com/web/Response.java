9308fe6b849afe19f150b73c5532ea6ef4b22b43
taewan625 <taewan1997625@gmail.com>
2024-08-06 23:02:10 +0900

    Feat : Add Response.java
    콘솔 프로젝트로 Response를 사용할 경우가 없다고 판단되지만 구조적인 학습을 위해 생성

---
+++ src/com/web/Response.java
@@ -0,0 +1,27 @@
+package com.web;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.utl.Parser;
+
+/**
+ * @Class Name : Response.java
+ * @Description 응답 객체 
+ * 
+ * @version 1.0
+ * @author 권태완
+ * @Since 2024.08.06
+ * @Modification Information
+ * @see Copyright (C) All right reserved.
+ */
+public class Response {
+	//전체 url
+	private String url;
+	
+	//요청 데이터
+	private Map<String, Object> clientDatas = new HashMap<>();
+	
+
+}
