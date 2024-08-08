9b0018bf3a4f0ff45b4192d998961c54ad666c1a
taewan625 <taewan1997625@gmail.com>
2024-08-09 01:10:51 +0900

    Refactor : Comment 주석 및 변수명

--- src/com/web/ViewResolver.java
+++ src/com/web/ViewResolver.java
@@ -1,28 +1,26 @@
 package com.web;
 
 /**
- * @Class Name : JavaViewResolver.java
- * @Description ViewResolver는 view template마다의 방식으로 view 객체를 반환.
- *              JavaViewResolver는 콘솔창으로 값을 반환하기 때문에 View반환형식은 ConsoleView라는
- *              인터페이스뷰를 구현한 view 구현체들을 Reflection API기술로 구현한다.
+ * @Class Name : ViewResolver.java
+ * @Description ViewResolver는 view template 마다의 방식으로 view 객체를 반환 | ps. 각 view Template은 인터페이스로 View를 구현한다.
+ *              View 객체란 : login.jsp 파일과 같은 특정 파일을 찾는 것이 아니라, 뷰를 나타내는 추상적인 객체를 찾는다.
+ *              Spring 구조에서는 ViewResolver 인터페이스를 구현한 여러 종류의 ViewResolver객체들이 존재하고 선택된 viewResovler가 동작하는 방식
  * @version 1.0
  * @author 권태완
  * @Since 2023.12.22.
  * @Modification Information
  * @see Copyright (C) All right reserved.
  */
-public class JavaViewResolver {
+public class ViewResolver {
 	/**
-	 * Func : 논리 view명을 물리 view명으로 변환
-	 * 
-	 * @desc 논리 view명을 물리 view명으로 변환
-	 * @param String
-	 *            logicalViewName
-	 * @return ViewTemplate
+	 * @desc 논리 view 경로를 물리 view 경로로 변환 후 View 객체 반환		예) JSP View 구현체 - InternalResourceView, Thymeleaf View 구현체 - ThymeleafView
+	 *       java 클래스로 만든 view를 처리해주는 JavaView 내가 생성한 JavaView를 반환
+	 * @param String logicalViewPath
+	 * @return JavaView
 	 * @throws Exception
 	 */
-	public static JavaView resolveView(String logicalViewName) throws Exception {
-		return new JavaView("view." + logicalViewName);
+	public static JavaView resolveView(String logicalViewPath) throws Exception {
+		return new JavaView("view." + logicalViewPath);
 	}
 
 }
