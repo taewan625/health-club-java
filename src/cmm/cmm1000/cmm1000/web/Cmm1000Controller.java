aec0a0633c9d0d80d988b1e08c8b89c8716db874
taewan625 <taewan1997625@gmail.com>
2024-08-18 01:52:42 +0900

    Refacotr : "메인 화면 view의 요청 구조 변경 및 팝업 구조 변경"
    TODO. 검증 기능 추가 변경 예정

--- src/cmm/cmm1000/cmm1000/web/Cmm1000Controller.java
+++ src/cmm/cmm1000/cmm1000/web/Cmm1000Controller.java
@@ -1,12 +1,8 @@
 package cmm.cmm1000.cmm1000.web;
 
-import java.lang.reflect.Method;
-import java.util.Properties;
-
-import com.propertiesconvert.MessageSource;
 import com.web.ModelView;
-import com.web.ModelViewController;
 import com.web.Request;
+import com.web.Response;
 
 /**
  * @Class Name : Cmm1000Controller.java
@@ -17,17 +13,14 @@ import com.web.Request;
  * @Modification Information
  * @see Copyright (C) All right reserved.
  */
-public class Cmm1000Controller implements ModelViewController {
-	// 메시지 프로퍼티스
-	public static Properties message = MessageSource.properties;
+public class Cmm1000Controller {
 	// 싱글톤
 	private static volatile Cmm1000Controller instance;
 
 	/**
 	 * Func : 싱글톤 객체 반환 메서드
 	 * 
-	 * @desc : 객체가 이미 존재하면 존재하는 객체를 반환하고 없으면 생성 후 반환. 최초 초기화만 동기화 작업이 일어나서 리소스 낭비를
-	 *       최소화
+	 * @desc : 객체가 이미 존재하면 존재하는 객체를 반환하고 없으면 생성 후 반환. 최초 초기화만 동기화 작업이 일어나서 리소스 낭비를 최소화
 	 * @param
 	 * @return Cmm1000Controller
 	 * @throws Exception
@@ -46,42 +39,12 @@ public class Cmm1000Controller implements ModelViewController {
 	}
 
 	/**
-	 * Func : Reflection API를 활용한 동적으로 수행되는 메서드
-	 * 
-	 * @desc : 메서드명을 이용하여 사용할 메서드를 호출
-	 * @param List<String>
-	 *            methodDatas
-	 * @return ModelView
-	 * @throws Exception
-	 */
-	@Override
-	public ModelView findMethod(Request request) throws Exception {
-		try {
-			// client 요청 데이터
-			String methodName = request.getMethodUrl();
-			Class<?> cmm1000Controller = Cmm1000Controller.class;
-			// 메뉴 메서드 호출
-			Method cmm1000ControllerMethod = cmm1000Controller.getMethod(methodName);
-			// 메서드 실행
-			Object result = cmm1000ControllerMethod.invoke(instance);
-
-			return (ModelView) result;
-		} catch (Exception e) {
-			ModelView modelView = new ModelView(message.getProperty("ERROR.PAGE"));
-			modelView.setDatas(ERROR_KEY, message.getProperty("ERROR.NOT_FOUND"));
-			return modelView;
-		}
-	} 
-
-	/**
-	 * Func : menu view Path 반환 메서드
-	 * 
 	 * @desc : menu view Path 반환
 	 * @param
 	 * @return ModelView
 	 * @throws Exception
 	 */
-	public ModelView selectCmm1000List() throws Exception {
+	public ModelView selectCmm1000List(Request request, Response response) throws Exception {
 		return new ModelView("cmm.cmm1000.cmm1000.Cmm1000");
 	}
 }
--- src/view/cmm/cmm1000/cmm1000/Cmm1000.java
+++ src/view/cmm/cmm1000/cmm1000/Cmm1000.java
@@ -3,6 +3,7 @@ package view.cmm.cmm1000.cmm1000;
 import java.util.Collections;
 import java.util.Map;
 
+import com.val.Validator;
 import com.web.MessageSource;
 import com.web.Request;
 
@@ -25,25 +26,26 @@ public class Cmm1000 implements JavaHTML {
 	}
 
 	/**
-	 * @desc 화면을 보여주고 요청을 보낼 경우 해당 화면에서 dispatcherServlet으로 값이 전달이 된다.
-	 * @param Object data
-	 * @return ClientRequest<?>
+	 * @desc 화면을 보여주고 요청을 보낼 경우 해당 화면에서 webContainer로 값이 전달이 된다.
+	 * @param Request request
+	 * @return void
 	 * @throws Exception
 	 */
-	//TODO 화면 내용만 여기에 존재하고 실제 response 정보는 response와 startWeb에서 수행 필요
 	@Override
-	public void response(Request Request) throws Exception {
+	public void response(Request request) throws Exception {
 		while (true) {
-			System.out.println("헬스프로그램 메뉴");
-			//TODO JAVA HTML의 역할 : 접근해야하는 메뉴 url 경로 매핑해주는 역할 필요
-			//System.out.println("1. 회원관리, 2.사물함 관리, 3.회원통계, 4.프로그램 종료");
-			//System.in()
-			// 받은 번호와 JAVA HTML에 저장된 url 경로 매핑 후 요청 보내기
-			String inputData = Cmm1001.choiceMenu("1. 회원관리, 2.사물함 관리, 3.회원통계, 4.프로그램 종료 \n메뉴의 번호를 입력하세요.", 1, 5);
+			//메인 메뉴 화면
+			System.out.println("헬스프로그램 메뉴\n1. 회원관리, 2.사물함 관리, 3.회원통계, 4.프로그램 종료 \n메뉴의 번호를 입력하세요.");
 
+			//client 입력값
+			String inputData = Validator.chooseRangeNum(1, 5);
+			
 			//시스템 종료
 			if (inputData.equals("4")) {
-				systemExit();
+				//종료 메시지
+				System.out.println("시스템이 종료 됩니다.");
+				//시스템 종료
+				System.exit(0);
 			} 
 			//URL 정보와 데이터를 객체에 담아서 WebContainer에 전송
 			else {
@@ -52,19 +54,8 @@ public class Cmm1000 implements JavaHTML {
 				
 				//WAS에 요청
 				webContainer.service(requestData);
-				
 			}
 		}
-	}
-
-	/**
-	 * Func : 시스템 종료 메서드
-	 * 
-	 * @desc 해당 스택에서 즉시 시스템을 종료 시킨다.
-	 */
-	private void systemExit() {
-		System.out.println("시스템이 종료 됩니다.");
-		System.exit(0);
 	}
 
 }
