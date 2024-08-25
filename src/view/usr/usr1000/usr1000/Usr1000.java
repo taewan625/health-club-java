41858d6e03aa389342c2c3e60a1e90d9d778760a
taewan625 <taewan1997625@gmail.com>
2024-08-25 23:27:30 +0900

    Refactor : 회원 메뉴, 회원 상세 정보 조회 화면 로직 수정

--- src/view/usr/usr1000/usr1000/Usr1000.java
+++ src/view/usr/usr1000/usr1000/Usr1000.java
@@ -1,20 +1,18 @@
 package view.usr.usr1000.usr1000;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 import com.val.Validator;
+import com.web.MessageSource;
 import com.web.Request;
 
 import usr.usr1000.usr1000.vo.Usr1000VO;
 import view.JavaHTML;
-import view.cmm.cmm1000.cmm1000.Cmm1001;
-import view.cmm.cmm1000.cmm1000.Cmm1004;
 
 /**
- * @Class Name : Usr1000.java
  * @Description 회원관리 리스트를 보여주는 클래스
  * @version 1.0
  * @author 권태완
@@ -25,155 +23,72 @@ import view.cmm.cmm1000.cmm1000.Cmm1004;
 public class Usr1000 implements JavaHTML {
 	private Scanner scanner = new Scanner(System.in);
 
-	/**
-	 * Func : 초기화 메서드가 존재하는 생성자
-	 * 
-	 * @desc :초기화 메서드의 예외를 처리하기 위해 try-catch와 new RuntimeExceptioin()을
-	 *       던진다. @param @return @throws
-	 */
+	//생성자
 	public Usr1000() {
-		try {
-			initUrl();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
+		urlMap.put("1", MessageSource.getMessage("message.usrS"));
+		urlMap.put("2", MessageSource.getMessage("message.usrC"));
+		urlMap.put("3", MessageSource.getMessage("message.usrUF"));
+		urlMap.put("4", MessageSource.getMessage("message.usrDF"));
+		urlMap.put("5", MessageSource.getMessage("message.menu"));
 	}
 
 	/**
-	 * Func : url 초기화 메서드
-	 * 
-	 * @desc JavaHTML interface에서 Map<String, String>을 담았기 때문에 이동할 url 경로만 초기화 해주면
-	 *       된다. 해당 value 사용시, urlMacth로 선언된 default 메서드를 사용하면된다.
-	 * @param
-	 * @return void
-	 * @throws Exception
-	 */
-	private void initUrl() throws Exception {
-		urlMap.put("1", msg.getProperty("usrS"));
-		urlMap.put("2", msg.getProperty("usrC"));
-		urlMap.put("3", msg.getProperty("usrUF"));
-		urlMap.put("4", msg.getProperty("usrDF"));
-		urlMap.put("5", msg.getProperty("menu"));
-	}
-
-	/**
-	 * Func : view 실행 메서드
-	 * 
 	 * @desc model을 가지고 있는 메서드, 메뉴는 포함관계를 이용하여 보여준다.
-	 * @param Request
-	 *            requestWithResponseData
+	 * @param Request requestWithResponseData
 	 * @return void
 	 * @throws Exception
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
-	public void response(Request requestWithResponseData) throws Exception {
+	public void response(Request request) throws Exception {
 		while (true) {
-			Cmm1004.failReason(requestWithResponseData);
-			// 회원리스트를 보여준다.
-			Map<String, ?> responseData = requestWithResponseData.getResponseData();
-			List<Usr1000VO> data = (List<Usr1000VO>) responseData.get("users");
-			showUsrList(data);
-			// 메뉴선택 공통 팝업창
-			String inputData = Cmm1001.choiceMenu("1.회원조회 2.회원 등록 3.회원 수정 4.회원 삭제 5.menu \n이동하실 메뉴의 번호를 입력하세요.", 1, 6);
-			// 새로운 request에 값을 담는다.
-			Request request = new Request(urlMatch(inputData));
-			// 조회, 수정, 삭제 시, 아이디를 작성하세요
-			List<String> userIds = getUserIds(data);
-			writeId(inputData, request, userIds);
-		}
-	}
-
-	/**
-	 * Func : 조회, 수정, 삭제 시, id를 물어봐서 request에 담는 메서드
-	 * 
-	 * @desc 조회, 수정, 삭제 시, id를 물어봐서 request에 담는다.
-	 * @param String
-	 *            inputData, Request request
-	 * @return void
-	 * @throws Exception
-	 */
-	private void writeId(String inputData, Request request, List<String> userIds) throws Exception {
-		switch (inputData) {
-		case "1":
-			setId("조회할 회원 id를 작성해주세요", request, userIds);
-			dispatcherServlet.service(request);
-			break;
-		case "2":
-			dispatcherServlet.service(request);
-			break;
-		case "3":
-			setId("수정하실 회원 id를 작성해주세요", request, userIds);
-			dispatcherServlet.service(request);
-			break;
-		case "4":
-			setId("삭제하실 회원 id를 작성해주세요", request, userIds);
-			dispatcherServlet.service(request);
-			break;
-		case "5":
-			dispatcherServlet.service(request);
-		}
-	}
-
-	/**
-	 * Func : id를 request 담는 메서드
-	 * 
-	 * @desc id를 request 담는 메서드로 코드 중복 방지
-	 * @param String
-	 *            prompt, Request request, List<String> userIds
-	 * @return void
-	 * @throws Exception
-	 */
-	private void setId(String prompt, Request request, List<String> userIds) throws Exception {
-		try {
-			System.out.println(prompt);
-			String id = scanner.next();
-			while (!Validator.isDuplicateId(id, userIds)) {
-				System.out.println("존자하지 않는 id 입니다.");
-				id = scanner.next();
+			//회원 목록 정보 TODO 페이징 처리
+			System.out.println("[회원 목록] - 1 페이지");
+			
+			//데이터 조회
+			Map<String, Object> datas = request.getDatas();
+			
+			//회원리스트 데이터 조회
+			List<Usr1000VO> users = (List<Usr1000VO>) datas.get("users");
+			
+			//회원 목록 출력
+			for (Usr1000VO user : users) {
+				System.out.println(user.toString());
 			}
-			request.setClientData("id", id);
-		} catch (Exception e) {
-			System.out.println("view/Usr1000/getUserIds() Error : " + e.getMessage());
-		}
-	}
-
-	/**
-	 * Func : 회원 id 모음 반환 메서드
-	 * 
-	 * @desc 회원 id 모음 반환
-	 * @param List<Usr1000VO>
-	 *            data
-	 * @return List<String>
-	 * @throws Exception
-	 */
-	private List<String> getUserIds(List<Usr1000VO> data) throws Exception {
-		try {
-			List<String> userIds = new ArrayList<>();
-			for (Usr1000VO user : data) {
-				userIds.add(user.getId());
+			
+			//회원 화면
+			System.out.println("1.회원조회 2.회원 등록 3.회원 수정 4.회원 삭제 5.메인메뉴");
+			
+			//client 입력값
+			String inputData = Validator.chooseRangeNum(1, 5);
+			
+			//조회, 수정, 삭제 시, 아이디 정보 추가
+			if ("1".equals(inputData) || "3".equals(inputData) || "4".equals(inputData)) {
+				//입력할 id
+				String userId = "";
+				
+				do {
+					//질문
+					System.out.println("아이디를 작성해주세요.");
+					
+					//데이터 입력
+					userId = scanner.nextLine().trim();
+					
+				} while("".equals(userId));
+				
+				//사용자 정보를 map에 담기
+				Map<String, String> userInfo =new HashMap<String, String>();// Map.of("userId", userId);
+				userInfo.put("userId", userId);
+				//요청할 객체에 담기
+				requestData.put("clientDatas", userInfo);
 			}
-			return userIds;
-		} catch (Exception e) {
-			System.out.println("view/Usr1000/getUserIds() Error : " + e.getMessage());
-			throw new Exception(e);
+			
+			//전송 객체 세팅
+			requestData.put("url", urlMatch(inputData));
+			
+			//WAS에 요청
+			webContainer.service(requestData);
 		}
 	}
-
-	/**
-	 * Func :userList 를 보여주는 메서드
-	 * 
-	 * @desc id,name 정보를 보여주는 userList 메서드
-	 * @param List<Usr1000VO>
-	 *            users
-	 * @return void
-	 * @throws Exception
-	 */
-	private void showUsrList(List<Usr1000VO> users) throws Exception {
-		int index = 1;
-		for (Usr1000VO user : users) {
-			System.out.println((index++) + " " + user.toStringUsrList());
-		}
-	}
-
+	
 }
--- src/view/usr/usr1000/usr1000/Usr1001.java
+++ src/view/usr/usr1000/usr1000/Usr1001.java
@@ -3,13 +3,13 @@ package view.usr.usr1000.usr1000;
 import java.util.Map;
 import java.util.Scanner;
 
+import com.web.MessageSource;
 import com.web.Request;
 
 import usr.usr1000.usr1000.vo.Usr1000VO;
 import view.JavaHTML;
 
 /**
- * @Class Name : Usr1001.java
  * @Description 회원정보를 보여주는 메서드
  * @version 1.0
  * @author 권태완
@@ -21,30 +21,38 @@ public class Usr1001 implements JavaHTML {
 	private Scanner scanner = new Scanner(System.in);
 
 	/**
-	 * Func : 회원 조회페이지를 보여주는 메서드
-	 * 
-	 * @desc 회원 조회 정보를 보여준다. 서버로부터 온 요청 값 중에서 err문구가 존재시, 예외 문구를 보여주고 user 메인 화면으로
-	 *       이동한다.
-	 * @param Request
-	 *            requestWithResponseData
+	 * @desc 회원 정보 조회
+	 * @param Request request
 	 * @return void
 	 * @throws Exception
 	 */
 	@Override
-	public void response(Request requestWithResponseData) throws Exception {
-		Map<String, ?> responseData = requestWithResponseData.getResponseData();
-
-		Usr1000VO user = (Usr1000VO) responseData.get("user");
-		// 예외 존재시
-		if (user.getErrCode().equals("-1")) {
-			System.out.println(user.getErrMsg());
-			System.out.println("조회한 회원이 없기 때문에 회원 관리 화면으로 돌아갑니다.");
-			return;
+	public void response(Request request) throws Exception {
+		System.out.println("[회원 상세]");
+		
+		//데이터 조회
+		Map<String, Object> datas = request.getDatas();
+		
+		//회원 정보 조회
+		Usr1000VO user = (Usr1000VO) datas.get("user");
+		
+		//회원 정보가 없는 경우
+		if (user == null) {
+			System.out.println("존재하지 않는 회원입니다.");
 		}
-		// 정상 로직 수행
-		System.out.println(user.toString());
-		// 메뉴로 돌아가기
-		System.out.println("회원관리페이지로 돌아가실려면 아무 키나 누르면 돌아갑니다.");
+		//회원 정보가 있는 경우
+		else {
+			System.out.println(user.toString());
+		}
+		
+		System.out.println("닫기 - 아무키나 누르세요");
 		scanner.nextLine();
+		
+		//회원 메뉴로 이동하는 전송 객체 생성
+		requestData.put("url", MessageSource.getMessage("message.usr"));
+		
+		//WAS에 요청
+		webContainer.service(requestData);
+		
 	}
 }
