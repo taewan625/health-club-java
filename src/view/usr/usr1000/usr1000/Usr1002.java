c740a7bf5a5e9146d518e664f801015dcebd0a05
taewan625 <taewan1997625@gmail.com>
2024-08-31 21:21:49 +0900

    Refactor : 회원 등록의 반복되는 do-while 로직을 메서드를 추출하여 코드 리팩토링 수행
    항목별 달라지는 검증 방법은 Predicate를 활용
    
    233line -> 178line (55line 감축) 및 코드 가독성 향상 효과

--- src/view/usr/usr1000/usr1000/Usr1002.java
+++ src/view/usr/usr1000/usr1000/Usr1002.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import com.val.Validator;
 import com.web.Request;
@@ -42,160 +44,78 @@ public class Usr1002 implements JavaHTML {
 		Usr1000VO userInfo = new Usr1000VO();
 		System.out.println("[회원 등록]");
 		
-		//중복 혹은 유효성에 문제가 존재하는지 여부 확인 변수
-		boolean isWrong = false;
-		
 		/*회원 등록 로직*/
 		//회원 id
-		do {
-			//id 작성
-			System.out.println("* 아이디 작성하세요. [중복 불가, 최소 4자 이상 작성] [회원 등록을 멈추고 나가고 싶으면 n 작성]");
-			
-			//입력 id 값
-			String userId = scanner.nextLine().trim();
-			
-			//중간 종료 여부 확인
-			wantStop(userId);
-			
-			//회원 아이디 중복 확인
-			isWrong = users.stream().map(Usr1000VO::getId).anyMatch(id -> id.equals(userId));
-			
-			//id가 중복 될 경우 문구 
-			if (isWrong) {
-				System.out.println("중복된 id가 존재합니다.");
-			}
-			//map에 데이터 담기
-			else {
-				userInfo.setId(userId);
-			}
-			
-		} while(isWrong);
+		String userId = execute("* 아이디 작성하세요. [중복 불가, 최소 4자 이상 작성] [회원 등록을 멈추고 나가고 싶으면 n 작성]"
+				, "중복된 id가 존재합니다."
+				, answer -> users.stream().map(Usr1000VO::getId).anyMatch(id -> id.equals(answer)));
+		
+		userInfo.setId(userId);
 		
 		//회원명
-		do {
-			System.out.println("* 이름을 작성하세요. [회원 등록을 멈추고 나가고 싶으면 n 작성]");
-			
-			//회원명 입력 값
-			String userName = scanner.nextLine().trim();
-			
-			//중간 종료 여부 확인
-			wantStop(userName);
-			
-			//유효성 검증 - 빈값 || 한글만 있거나 영어만 있거나
-			isWrong = userName.isEmpty() || !(Validator.isKorean(userName) || Validator.isEnglish(userName));
-			
-			//id가 중복 될 경우 문구 
-			if (isWrong) {
-				System.out.println("이름은 영어 혹은 한글만 가능합니다.");
-			}
-			//map에 데이터 담기
-			else {
-				userInfo.setName(userName);
-			}
-			
-		} while (isWrong);
+		String userName = execute("* 이름을 작성하세요. [회원 등록을 멈추고 나가고 싶으면 n 작성]"
+				, "이름은 영어 혹은 한글만 가능합니다."
+				, answer -> answer.isEmpty() || !(Validator.isKorean(answer) || Validator.isEnglish(answer)));
 		
-		//성별 선택
-		do {
-			System.out.println("* 성별을 작성하세요. [회원 등록을 멈추고 나가고 싶으면 n 작성]");
-			
-			//성별 입력 값
-			String gender = scanner.nextLine().trim().toLowerCase();
-			
-			//중간 종료 여부 확인
-			wantStop(gender);
-			
-			//유효성 검증 - f, m 이 아닌 경우
-			isWrong = !("f".equals(gender) || "m".equals(gender));
-			
-			//id가 중복 될 경우 문구 
-			if (isWrong) {
-				System.out.println("올바른 성별을 작성하세요.");
-			}
-			//map에 데이터 담기
-			else {
-				userInfo.setGender(gender);
-			}
-			
-		} while(isWrong);
+		userInfo.setName(userName);
 		
-		//연락처 작성
-		do {
-			System.out.println("* 연락처를 작성하세요. [010-xxxx-xxxx 형식으로 작성] [회원 등록을 멈추고 나가고 싶으면 n 작성]");
-			
-			//연락처 입력 값
-			String phoneNumber = scanner.nextLine().trim();
-			
-			//중간 종료 여부 확인
-			wantStop(phoneNumber);
-			
-			//유효성 검증
-			isWrong = !Validator.isValidPhoneNumber(phoneNumber);
-			
-			//id가 중복 될 경우 문구 
-			if (isWrong) {
-				System.out.println("올바른 연락처를 작성하세요.");
-			}
-			//map에 데이터 담기
-			else {
-				userInfo.setPhoneNumber(phoneNumber);
-			}
-			
-		} while(isWrong);
+		//성별
+		String gender = execute("* 성별을 작성하세요. [m | f] [회원 등록을 멈추고 나가고 싶으면 n 작성]"
+				, "올바른 성별을 작성하세요. [m | f]"
+				, answer -> !("f".equals(answer) || "m".equals(answer)));
+		
+		userInfo.setGender(gender);
+		
+		//연락처
+		String phoneNumber = execute("* 연락처를 작성하세요. [010-xxxx-xxxx 형식으로 작성] [회원 등록을 멈추고 나가고 싶으면 n 작성]"
+				, "올바른 연락처를 작성하세요."
+				, answer -> !Pattern.matches("^010-\\d{4}-\\d{4}$", answer));
+		
+		userInfo.setPhoneNumber(phoneNumber);
 		
 		//주소 작성
-		System.out.println("주소를 작성하세요. [주소는 필수 값이 아닙니다.] [회원 등록을 멈추고 나가고 싶으면 n 작성]");
+		String address = execute("주소를 작성하세요. [주소는 필수 값이 아닙니다.] [회원 등록을 멈추고 나가고 싶으면 n 작성]"
+				, null
+				, input -> false);
 		
-		//주소 입력 값
-		String address = scanner.nextLine().trim();
-		
-		//중간 종료 여부 확인
-		wantStop(address);
-		
-		//map에 데이터 담기
 		userInfo.setAddress(address);
 		
 		//회원 설명
-		System.out.println("회원 설명을 작성하세요. [회원 설명은 필수 값이 아닙니다.] [회원 등록을 멈추고 나가고 싶으면 n 작성]");
+		String description = execute("회원 설명을 작성하세요. [회원 설명은 필수 값이 아닙니다.] [회원 등록을 멈추고 나가고 싶으면 n 작성]"
+				, null
+				, input -> false);
 		
-		//회원 설명 입력 값
-		String description = scanner.nextLine().trim();
-		
-		//중간 종료 여부 확인
-		wantStop(description);
-		
-		//map에 데이터 담기
 		userInfo.setDescription(description);
-		//TODO. 회원 등록일 유효성 검사 필요함. do-while 사용필요
+		
 		//회원 등록일
-		System.out.println("*회원 등록일을 작성하세요. [2024-08-28 형식으로 작성] [회원 등록을 멈추고 나가고 싶으면 n 작성]");
+		String joinDate = execute("*회원 등록일을 작성하세요. [2024-08-28 형식으로 작성] [회원 등록을 멈추고 나가고 싶으면 n 작성]"
+				, "금일 이후 올바른 일자를 작성하세요."
+				//TODO. 유효성 - 올바른 형식, 오늘 포함 이후 일자의 유효성 검사 필요
+				, input -> false);
 		
-		//회원 등록일 입력 값
-		String joinDate = scanner.nextLine().trim();
-		
-		//중간 종료 여부 확인
-		wantStop(joinDate);
-		
-		//map에 데이터 담기
 		userInfo.setJoinDate(LocalDate.parse(joinDate));
 		
-		//TODO. 회원 만료일 유효성 검사 필요함. do-while 사용필요
-		System.out.println("*회원 만료일을 작성하세요. [2024-08-28 형식으로 작성] [회원 등록을 멈추고 나가고 싶으면 n 작성]");
+		//회원 등록일
+		String expireDate = execute("*회원 만료일을 작성하세요. [2024-08-28 형식으로 작성] [회원 등록을 멈추고 나가고 싶으면 n 작성]"
+				, "금일 이후 올바른 일자를 작성하세요."
+				//TODO. 유효성 - 올바른 형식, 등록일보다 이후 일자
+				, input -> false);
 		
-		//회원 만료일 입력 값
-		String expireDate = scanner.nextLine().trim();
-		
-		//중간 종료 여부 확인
-		wantStop(expireDate);
-		
-		//map에 데이터 담기
 		userInfo.setExpireDate(LocalDate.parse(expireDate));
 		
-		//메타정보 넣기
+		//사용 유무 정보
 		userInfo.setUse("Y");
+		
+		//상태 정보 [정상, 임박, 만기]
 		userInfo.setStatus("정상");
+		
+		//삭제 유무 정보 TODO 제거
 		userInfo.setDelete("N");
+		
+		//최초등록 메타 정보 TODO 명칭 변경 register, modification
 		userInfo.setJoinDateTime(LocalDateTime.now());
+		
+		//최종수정 메타 정보 TODO 명칭 변경 register, modification
 		userInfo.setEditDateTime(LocalDateTime.now());
 		
 		//전송용 데이터 객체에 담기
@@ -210,23 +130,48 @@ public class Usr1002 implements JavaHTML {
 		//WAS에 요청
 		webContainer.service(requestData);
 	}
-
-
+	
 	/**
-	 * @desc 회원등록을 멈추고 회원 화면 메뉴로 이동
-	 * @param String inputValue
-	 * @return void
+	 * @desc 회원 등록시 검증 작업 및 회원 등록 취소 작업 수행
+	 * @param String questionPromp, String warningPromp, Predicate<String> validate
+	 * @return String
 	 * @throws Exception
 	 */
-	private void wantStop(String inputValue) throws Exception {
-		//대소문자 구분 없이 n을 작성할 경우 회원 화면으로 이동
-		if ("n".equals(inputValue) || "N".equals(inputValue)) {
-			//회원 메뉴로 이동하는 전송 객체 생성
-			requestData.put("url", "usr/usr1000/usr1000/selectUsr1000View");
+	private String execute(String questionPromp, String warningPromp, Predicate<String> validate) throws Exception {
+		//중복 혹은 유효성에 문제가 존재하는지 여부 확인 변수
+		boolean isWrong = false;
+		
+		//반환 될 사용자 입력값 변수
+		String answer;
+		
+		do {
+			//질문
+			System.out.println(questionPromp);
 			
-			//WAS에 요청
-			webContainer.service(requestData);
-		}
+			//사용자 입력값
+			answer = scanner.nextLine().trim();
+			
+			//대소문자 구분 없이 n을 작성할 경우 회원 화면으로 이동
+			if ("n".equals(answer) || "N".equals(answer)) {
+				//회원 메뉴로 이동하는 전송 객체 생성
+				requestData.put("url", "usr/usr1000/usr1000/selectUsr1000View");
+				
+				//WAS에 요청
+				webContainer.service(requestData);
+			}
+			
+			//제약조건 검사
+			isWrong = validate.test(answer);
+			
+			//입력값에 오류가 존재할 경우 경고문 
+			if (isWrong) {
+				System.out.println(warningPromp);
+			}
+			
+		} while(isWrong);
+		
+		//사용자 입력값 반환
+		return answer;
 	}
 
 }
