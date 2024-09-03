cb9cbf20dc794657da1e97a79b350d4667bed9f6
taewan625 <taewan1997625@gmail.com>
2024-09-04 02:04:47 +0900

    Refactor : "사물함 목록 조회 및 사물함 단건 조회 로직 수정"
    사물함 목록 조회 : 단순 목록 조회
    사물함 단건 조회 : 사용자 편의성을 고려하여 사물함 번호 및 사용자 아이디로 조회 할 수 있도록 기능 추가

--- src/lck/lck1000/lck1000/service/impl/Lck1000DAO.java
+++ src/lck/lck1000/lck1000/service/impl/Lck1000DAO.java
@@ -5,12 +5,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import com.propertiesconvert.MessageSource;
 import com.val.Validator;
 
 import lck.lck1000.lck1000.vo.Lck1000VO;
 import memorydb.LCK1000;
-import memorydb.domain.Locker;
+import memorydb.USR1000;
+import usr.usr1000.usr1000.vo.Usr1000VO;
 
 /**
  * @Class Name : Lck1000DAO.java
@@ -22,230 +22,221 @@ import memorydb.domain.Locker;
  * @see Copyright (C) All right reserved.
  */
 public class Lck1000DAO {
-	public static Properties message = MessageSource.properties;
-	// db table
-	private Map<Integer, Locker> lockerStore;
-
-	/**
-	 * @Class Name Lck1000DAOHolder 내부 클래스
-	 * @Description 싱글톤 객체 생성 - 다중 접근 null 방지 및 동기화 성능 향상 목적 클래스
-	 * @version 1.0
-	 * @author 권태완
-	 * @Since 2023.12.26.
-	 * @Modification Information
-	 * @see Copyright (C) All right reserved.
-	 */
+	//사물함 db table
+	private Map<Integer, Lck1000VO> lockerStore = LCK1000.getInstance().getLck1000();
+	
+	//싱글톤 내부 클래스
 	private static class Lck1000DAOHolder {
 		private static final Lck1000DAO INSTANCE = new Lck1000DAO();
 	}
-
-	/**
-	 * Func : 싱글톤 객체 반환 메서드
-	 * 
-	 * @desc 싱글톤 객체 반환
-	 * @param
-	 * @return Map<Integer, Locker>
-	 * @throws Exception
-	 */
+	
+	//싱글톤
 	public static Lck1000DAO getInstance() throws Exception {
 		return Lck1000DAOHolder.INSTANCE;
 	}
 
-	/**
-	 * Func : 싱글톤 객체 반환 메서드
-	 * 
-	 * @desc 싱글톤 객체 반환
-	 * @param LCK1000
-	 *            db
-	 * @return void
-	 * @throws Exception
-	 */
-	public void setDBTable(LCK1000 db) throws Exception {
-		try {
-			this.lockerStore = LCK1000.getInstance().lockerStore;
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
 
 	/**
-	 * Func : 사물함 리스트 반환 메서드
-	 * 
-	 * @desc 사물함 리스트 반환
-	 * @param
+	 * @desc 사물함 목록 반환
+	 * @param Map<String, String> pageInfo
 	 * @return List<Lck1000VO>
 	 * @throws Exception
 	 */
-	public List<Lck1000VO> selectLck1000List() throws Exception {
-		try {
-			List<Lck1000VO> lockers = new ArrayList<>();
-			for (Locker value : lockerStore.values()) {
-				lockers.add(new Lck1000VO(value.getUserId(), value.getLockerNumber(), value.getExpireDate()));
-			}
-			return lockers;
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
+	public List<Lck1000VO> selectLck1000List(Map<String, String> pageInfo) throws Exception {
+		ArrayList<Lck1000VO> lockers = new ArrayList<>(lockerStore.values());
+		
+		//선택한 페이지
+		String selectPage = pageInfo.get("selectPage");
+		
+		//시작 범위
+		int from = (Integer.parseInt(selectPage) - 1) * Integer.parseInt(pageInfo.get("range"));
+		
+		//종료 범위
+		int to = (Integer.parseInt(selectPage)) * Integer.parseInt(pageInfo.get("range"));
+		
+		//종료 범위가 토탈 개수보다 작을 경우
+		to = (lockers.size() < to) ? lockers.size() : to;
+		
+		//조회 사물함 목록
+		return lockers.subList(from, to);
 	}
-
+	
 	/**
-	 * Func : 회원 id로 사물함 정보 반환 메서드
-	 * 
-	 * @desc 회원 id 이용해서 사물함 정보 반환, 없으면 예외 발생, 해당 메서드 수행전, 회원 id를 가진 사물함 존재 유무 확인 필수
-	 * @param String
-	 *            userId
+	 * @desc 사물함 조회
+	 * @param String selectType, String selectValue
 	 * @return Lck1000VO
 	 * @throws Exception
-	 * 
 	 */
-	public Lck1000VO selectLck1000(String userId) throws Exception {
-		try {
-			for (Locker locker : lockerStore.values()) {
-				// 회원 부분은 null일 수 있다.
-				if (locker.getUserId() == null) {
-					continue;
-				}
-				// 회원이 null이 아니고 userId가 동일한 경우 Lck1000VO 반환
-				if (locker.getUserId().equals(userId)) {
-					return new Lck1000VO(userId, locker.getLockerNumber(), locker.getExpireDate());
-				}
-			}
-			throw new Exception(message.getProperty("ERROR.SELECT"));
-		} catch (Exception e) {
-			throw new Exception(e);
+	public Lck1000VO selectLck1000(String selectType, String selectValue) throws Exception {
+		//반환할 사물함 변수
+		Lck1000VO locker = null;
+		
+		//사물함 번호로 조회하는 경우
+		if ("1".equals(selectType)) {
+			locker = lockerStore.get(Integer.parseInt(selectValue));
+			
 		}
-	}
-
-	/**
-	 * Func : 사물함 번호로 사물함 정보 반환 메서드
-	 * 
-	 * @desc 사물함 번호를 이용해서 사물함 정보 반환, 해당 메서드 수행전 사물함 번호 존재 유무 확인 필수
-	 * @param int
-	 *            lockerNumber
-	 * @return Lck1000VO
-	 * @throws Exception
-	 * 
-	 */
-	public Lck1000VO selectLck1000(int lockerNumber) throws Exception {
-		try {
-			// db에서 view 역할 수행
-			Locker selectLocker = selectLocker(lockerNumber);
-			return new Lck1000VO(selectLocker.getUserId(), lockerNumber, selectLocker.getExpireDate());
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
-
-	/**
-	 * Func : 사물함 등록, 수정 메서드
-	 * 
-	 * @desc 회원 id 를 넣어서 등록한다. 등록전 사물함번호 존재 여부 확인 필수
-	 * @param Lck1000VO
-	 *            lck1000vo
-	 * @return void
-	 * @throws Exception
-	 */
-	public void saveLck1000(Lck1000VO lck1000VO) throws Exception {
-		try {
-			int lockerNum = lck1000VO.getLockerNum();
-			Locker registerLocker = selectLocker(lockerNum);
-
-			registerLocker.setUserId(lck1000VO.getUserId());
-			registerLocker.setExpireDate(lck1000VO.getExpireDate());
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
-
-	/**
-	 * Func : 만료된 사물함 회원 id 및 만료일 제거 메서드
-	 * 
-	 * @desc 만료된 사물함의 회원id, 사물함 만료일 제거 후 true 반환, 해당 메서드 사용전 사물함 번호 존재 유무 확인 필수
-	 * @param int
-	 *            lockerNum
-	 * @return Locker
-	 * @throws Exception
-	 */
-	public void deleteLck1000(int lockerNum) throws Exception {
-		try {
-			Locker locker = selectLocker(lockerNum);
-			locker.setUserId(null);
-			locker.setExpireDate(null);
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
-
-	/**
-	 * Func : 회원 id를 가진 사물함 존재 유무 확인 메서드
-	 * 
-	 * @desc 사물함에 회원명이 기입되고 사물함 만료일이 지나지 않았을 경우. cf) 회원이 만료되었는지, 제거 되었는지 유무는 회원측에서
-	 *       확인 필요. 따라서 해당 메서드 수행전 회원 사용유무부터 확인 필수
-	 * @param String
-	 *            userId
-	 * @return boolean
-	 * @throws Exception
-	 */
-	public boolean isContainsUsr1000(String userId) throws Exception {
-		try {
-			for (Locker locker : lockerStore.values()) {
-				if (locker.getUserId() == null) {
-					continue;
-				}
-				if (locker.getUserId().equals(userId)) {
-					// 사물함 만료기간 지났으면 회원이 없는 것
-					// 만료가 됨 == 회원이 없는 것 == false 반환
-					return !Validator.isExpire(locker.getExpireDate());
+		//회원 아이디로 조회하는 경우
+		else {
+			//사물함 정보 담기
+			ArrayList<Lck1000VO> lockers = new ArrayList<>(lockerStore.values());
+			
+			//반복 루프를 통해서 사물함 정보를 조회
+			for (Lck1000VO lockerInfo : lockers) {
+				//사물함 정보에 등록된 아이디와 조회 아이디 비교
+				if (lockerInfo.getUserId() == selectValue) {
+					//변수에 사물함 정보 담기
+					locker = lockerInfo;
+					
+					break;
 				}
 			}
-			return false;
-		} catch (Exception e) {
-			throw new Exception(e);
 		}
+		
+		//데이터 반환
+		return locker;
 	}
 
-	/**
-	 * Func : 사물함 번호 존재 유무 확인 메서드
-	 * 
-	 * @desc 존재하는 사물함 번호일 시 , true 반환
-	 * @param int
-	 *            lockerNumber
-	 * @return boolean
-	 * @throws Exception
-	 */
-	public boolean isContainsLck1000(int lockerNumber) throws Exception {
-		try {
-			for (Locker locker : lockerStore.values()) {
-				if (locker.getLockerNumber() == lockerNumber) {
-					return true;
-				}
-			}
-			return false;
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
+//	/**
+//	 * Func : 회원 id로 사물함 정보 반환 메서드
+//	 * 
+//	 * @desc 회원 id 이용해서 사물함 정보 반환, 없으면 예외 발생, 해당 메서드 수행전, 회원 id를 가진 사물함 존재 유무 확인 필수
+//	 * @param String
+//	 *            userId
+//	 * @return Lck1000VO
+//	 * @throws Exception
+//	 * 
+//	 */
+//	public Lck1000VO selectLck1000(String userId) throws Exception {
+//		try {
+//			for (Locker locker : lockerStore.values()) {
+//				// 회원 부분은 null일 수 있다.
+//				if (locker.getUserId() == null) {
+//					continue;
+//				}
+//				// 회원이 null이 아니고 userId가 동일한 경우 Lck1000VO 반환
+//				if (locker.getUserId().equals(userId)) {
+//					return new Lck1000VO(userId, locker.getLockerNumber(), locker.getExpireDate());
+//				}
+//			}
+//			throw new Exception(message.getProperty("ERROR.SELECT"));
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
+//
 
-	/**
-	 * Func : locker 반환 내부 메서드
-	 * 
-	 * @desc 사물함 번호를 이용해서 Locker 자체를 반환하는 메서드, 사용전 사물함 번호 존재유무 확인 필요
-	 * @param int
-	 *            lockerNumber
-	 * @return Locker
-	 * @throws Exception
-	 */
-	private Locker selectLocker(int lockerNumber) throws Exception {
-		try {
-			for (Locker locker : lockerStore.values()) {
-				if (locker.getLockerNumber() == lockerNumber) {
-					return locker;
-				}
-			}
-			throw new Exception(message.getProperty("ERROR.SELECT"));
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
+//
+//	/**
+//	 * Func : 사물함 등록, 수정 메서드
+//	 * 
+//	 * @desc 회원 id 를 넣어서 등록한다. 등록전 사물함번호 존재 여부 확인 필수
+//	 * @param Lck1000VO
+//	 *            lck1000vo
+//	 * @return void
+//	 * @throws Exception
+//	 */
+//	public void saveLck1000(Lck1000VO lck1000VO) throws Exception {
+//		try {
+//			int lockerNum = lck1000VO.getLockerNum();
+//			Locker registerLocker = selectLocker(lockerNum);
+//
+//			registerLocker.setUserId(lck1000VO.getUserId());
+//			registerLocker.setExpireDate(lck1000VO.getExpireDate());
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
+//
+//	/**
+//	 * Func : 만료된 사물함 회원 id 및 만료일 제거 메서드
+//	 * 
+//	 * @desc 만료된 사물함의 회원id, 사물함 만료일 제거 후 true 반환, 해당 메서드 사용전 사물함 번호 존재 유무 확인 필수
+//	 * @param int
+//	 *            lockerNum
+//	 * @return Locker
+//	 * @throws Exception
+//	 */
+//	public void deleteLck1000(int lockerNum) throws Exception {
+//		try {
+//			Locker locker = selectLocker(lockerNum);
+//			locker.setUserId(null);
+//			locker.setExpireDate(null);
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
+//
+//	/**
+//	 * Func : 회원 id를 가진 사물함 존재 유무 확인 메서드
+//	 * 
+//	 * @desc 사물함에 회원명이 기입되고 사물함 만료일이 지나지 않았을 경우. cf) 회원이 만료되었는지, 제거 되었는지 유무는 회원측에서
+//	 *       확인 필요. 따라서 해당 메서드 수행전 회원 사용유무부터 확인 필수
+//	 * @param String
+//	 *            userId
+//	 * @return boolean
+//	 * @throws Exception
+//	 */
+//	public boolean isContainsUsr1000(String userId) throws Exception {
+//		try {
+//			for (Locker locker : lockerStore.values()) {
+//				if (locker.getUserId() == null) {
+//					continue;
+//				}
+//				if (locker.getUserId().equals(userId)) {
+//					// 사물함 만료기간 지났으면 회원이 없는 것
+//					// 만료가 됨 == 회원이 없는 것 == false 반환
+//					return !Validator.isExpire(locker.getExpireDate());
+//				}
+//			}
+//			return false;
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
+//
+//	/**
+//	 * Func : 사물함 번호 존재 유무 확인 메서드
+//	 * 
+//	 * @desc 존재하는 사물함 번호일 시 , true 반환
+//	 * @param int
+//	 *            lockerNumber
+//	 * @return boolean
+//	 * @throws Exception
+//	 */
+//	public boolean isContainsLck1000(int lockerNumber) throws Exception {
+//		try {
+//			for (Locker locker : lockerStore.values()) {
+//				if (locker.getLockerNumber() == lockerNumber) {
+//					return true;
+//				}
+//			}
+//			return false;
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
+//
+//	/**
+//	 * Func : locker 반환 내부 메서드
+//	 * 
+//	 * @desc 사물함 번호를 이용해서 Locker 자체를 반환하는 메서드, 사용전 사물함 번호 존재유무 확인 필요
+//	 * @param int
+//	 *            lockerNumber
+//	 * @return Locker
+//	 * @throws Exception
+//	 */
+//	private Locker selectLocker(int lockerNumber) throws Exception {
+//		try {
+//			for (Locker locker : lockerStore.values()) {
+//				if (locker.getLockerNumber() == lockerNumber) {
+//					return locker;
+//				}
+//			}
+//			throw new Exception(message.getProperty("ERROR.SELECT"));
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
 
 }
--- src/lck/lck1000/lck1000/service/impl/Lck1000ServiceImpl.java
+++ src/lck/lck1000/lck1000/service/impl/Lck1000ServiceImpl.java
@@ -1,280 +1,255 @@
 package lck.lck1000.lck1000.service.impl;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import com.config.AppConfig;
-import com.propertiesconvert.MessageSource;
-import com.val.Validator;
 
 import lck.lck1000.lck1000.service.Lck1000Service;
 import lck.lck1000.lck1000.vo.Lck1000VO;
 import usr.usr1000.usr1000.service.impl.Usr1000DAO;
-import usr.usr1000.usr1000.vo.Usr1000VO;
 
 public class Lck1000ServiceImpl implements Lck1000Service {
-	public static Properties message = MessageSource.properties;
-
+	//생성자 주입
 	private final Lck1000DAO lck1000DAO;
+	//생성자 주입
 	private final Usr1000DAO usr1000DAO;
-
-	/**
-	 * Func 초기화 메서드를 담는 생성자
-	 * 
-	 * @desc 싱글톤 생성시, Map 초기화 예외 발생 경우 초기화 문제를 잡기 위해 try-catch 수행 main method에서 객체를
-	 *       초기화 할 경우 try-catch로 잡는 것과 같이 생성자 내부에서 초기화 수행시, try-catch로 잡는 형식으로
-	 *       생성 @param @return
-	 */
+	
+	//생성자
 	private Lck1000ServiceImpl(Lck1000DAO lck1000DAO, Usr1000DAO usr1000DAO) {
 		this.lck1000DAO = lck1000DAO;
 		this.usr1000DAO = usr1000DAO;
 	}
-
-	/**
-	 * @Class Name Usr1000ServiceImplHolder 내부 클래스
-	 * @Description 싱글톤 객체 생성 - 다중 접근 null 방지 및 동기화 성능 향상 목적 클래스
-	 * @version 1.0
-	 * @author 권태완
-	 * @Since 2023.12.23.
-	 * @Modification Information
-	 * @see Copyright (C) All right reserved.
-	 */
+	
+	//싱글톤 생성용 내부 클래스
 	private static class Lck1000ServiceImplHolder {
-		private static final Lck1000ServiceImpl INSTANCE = new Lck1000ServiceImpl(AppConfig.lck1000DAO(),
-				AppConfig.usr1000DAO());
+		private static final Lck1000ServiceImpl INSTANCE = new Lck1000ServiceImpl(AppConfig.lck1000DAO(), AppConfig.usr1000DAO());
 	}
-
-	/**
-	 * Func : 싱글톤 객체 반환 메서드
-	 * 
-	 * @desc 싱글톤 객체 반환
-	 * @param
-	 * @return Usr1000ServiceImpl
-	 * @throws Exception
-	 */
+	
+	//싱글톤
 	public static Lck1000ServiceImpl getInstance() throws Exception {
 		return Lck1000ServiceImplHolder.INSTANCE;
 	}
 
 	/**
-	 * Func : 사물함 화면의 default 정보를 보여주는 메서드
-	 * 
-	 * @desc 사물함 데이터중 회원 id, 사물함 번호, 만료일자 보여준다.
-	 * @param
+	 * @desc 사물함 목록 반환
+	 * @param Map<String, String> pageInfo
 	 * @return Map<Integer, Locker>
 	 * @throws Exception
 	 */
 	@Override
-	public List<Lck1000VO> selectLck1000List() throws Exception {
-		return lck1000DAO.selectLck1000List();
+	public List<Lck1000VO> selectLck1000List(Map<String, String> pageInfo) throws Exception {
+		return lck1000DAO.selectLck1000List(pageInfo);
 	}
-
+	
 	/**
-	 * Func : 요청 url의 key와 value의 값이 유효한지 확인하는 메서드
-	 * 
-	 * @desc key 존재 시, key의 value가 유효한지 확인한다.
-	 * @param String
-	 *            requestParams
-	 * @return boolean
-	 * @throws Exception
-	 */
-	@Override
-	public boolean isValidSelectKeyValue(Map<String, Object> clientData) throws Exception {
-		try {
-			if (clientData.containsKey("lockerNum")) {
-				// 존재하는 사물함 번호인지 검증. 값을 받아오기전 숫자 검증을 수행함.
-				String lockerNum = String.valueOf(clientData.get("lockerNum"));
-				return lck1000DAO.isContainsLck1000(Integer.parseInt(lockerNum));
-			} else {
-				// 사물함에 회원이 존재하는지 확인
-				return lck1000DAO.isContainsUsr1000(String.valueOf(clientData.get("id")));
-			}
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
-
-	/**
-	 * Func : 회원 id와 사물함 번호로 사물함 정보 반환하는 메서드
-	 * 
-	 * @desc url의 key 값의 종류에 따라서 값을 들고 온다.
-	 * @param String
-	 *            requestParams
+	 * @desc 사물함 단건 조회
+	 * @param String selectType, String selectValue
 	 * @return Lck1000VO
 	 * @throws Exception
 	 */
 	@Override
-	public Lck1000VO selectLck1000(Map<String, Object> clientData) throws Exception {
-		try {
-			// userId로 호출할 경우
-			if (clientData.containsKey("id")) {
-				return lck1000DAO.selectLck1000(String.valueOf(clientData.get("id")));
+	public Lck1000VO selectLck1000(String selectType, String selectValue) throws Exception {
+		//사물함 정보 조회
+		Lck1000VO locker = lck1000DAO.selectLck1000(selectType, selectValue);
+		
+		//사물함 정보와 사물함 만료일 정보가 비어있지 않고 만료일을 넘긴 경우
+		if (locker != null && locker.getEndDate() != null && locker.getEndDate().isBefore(LocalDate.now())) {
+			//해당 사물함 회원 정보 비우기
+			locker.setUserId(null);
+			
+			//헤당 사물함 등록일자 정보 비우
+			locker.setStartDate(null);
+			
+			//헤당 사물함 만료일자 정보 비우
+			locker.setEndDate(null);
+			
+			//TODO 사물함 정보 업데이트하기
+			
+			//조회 유형이 회원 아이디로 조회한 경우
+			if ("2".equals(selectType)) {
+				//전달할 사물함 정보 비우기
+				locker = null;
 			}
-			// 사물함 번호
-			String lockerNum = String.valueOf(clientData.get("lockerNum"));
-			// 사물함 번호로 호출하는 경우
-			Lck1000VO locker = lck1000DAO.selectLck1000(Integer.parseInt(lockerNum));
-
-			// 사물함 만료 기간이 지났으면 update 후 다시 받아와서 return
-			if (Validator.isExpire(locker.getExpireDate())) {
-				lck1000DAO.deleteLck1000(Integer.parseInt(lockerNum));
-				return lck1000DAO.selectLck1000(Integer.parseInt(lockerNum));
-			}
-			// 만료되지 않았으면 바로 return
-			return locker;
-		} catch (Exception e) {
-			throw new Exception(e);
 		}
-
+		
+		//사물함 정보 반환
+		return locker;
 	}
 
-	/**
-	 * Func : 회원 id 리스트 반환 메서드
-	 * 
-	 * @desc 회원 id 리스트 반환 메서드
-	 * @param
-	 * @return List<String>
-	 * @throws Exception
-	 */
-	@Override
-	public List<String> selectUsr1000List() throws Exception {
-		try {
-			List<Usr1000VO> selectUsr1000List = usr1000DAO.selectUsr1000List();
-			List<String> userIds = new ArrayList<>();
-			for (Usr1000VO user : selectUsr1000List) {
-				userIds.add(user.getId());
-			}
-			return userIds;
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
+//	/**
+//	 * Func : 요청 url의 key와 value의 값이 유효한지 확인하는 메서드
+//	 * 
+//	 * @desc key 존재 시, key의 value가 유효한지 확인한다.
+//	 * @param String
+//	 *            requestParams
+//	 * @return boolean
+//	 * @throws Exception
+//	 */
+//	@Override
+//	public boolean isValidSelectKeyValue(Map<String, Object> clientData) throws Exception {
+//		try {
+//			if (clientData.containsKey("lockerNum")) {
+//				// 존재하는 사물함 번호인지 검증. 값을 받아오기전 숫자 검증을 수행함.
+//				String lockerNum = String.valueOf(clientData.get("lockerNum"));
+//				return lck1000DAO.isContainsLck1000(Integer.parseInt(lockerNum));
+//			} else {
+//				// 사물함에 회원이 존재하는지 확인
+//				return lck1000DAO.isContainsUsr1000(String.valueOf(clientData.get("id")));
+//			}
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
+//
 
-	/**
-	 * Func : 사물함 등록 메서드
-	 * 
-	 * @desc 회원아이디가 존재하지 않고 비어있는 사물함 및 만료된 사물함에 한해서 등록 가능. 실패시 false 반환, 성공시 등록 후
-	 *       true 반환
-	 * @param Lck1000VO
-	 *            lck1000VO
-	 * @return boolean
-	 * @throws Exception
-	 */
-	@Override
-	public void createLck1000(Lck1000VO locker) throws Exception {
-		String userId = locker.getUserId();
-		try {
-			int lockerNum = locker.getLockerNum();
-
-			// 등록, 수정에서 공통으로 적용되는 검증 부분
-			if (commonValidate(userId, lockerNum, locker)) {
-				return;
-			}
-
-			// 넣으려는 사물함에 회원이 있고 만기일이 안넘으면 false
-			Lck1000VO exsitLocker = lck1000DAO.selectLck1000(lockerNum);
-
-			// 해당 사물함에 회원이 존재하고 사물함 만료일이 지나지 않으면 false
-			if (!exsitLocker.getUserId().isEmpty() && !Validator.isExpire(exsitLocker.getExpireDate())) {
-				locker.setError("-1", message.getProperty("FAIL.CREATE.LOCKER"));
-				return;
-			}
-
-			// 이제 true의 상황 밖에 없으니깐 여기서는 등록을 한다.
-			lck1000DAO.saveLck1000(locker);
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
-
-	/**
-	 * Func : 사물함 수정 메서드
-	 * 
-	 * @desc 사물함에 회원의 존재 여부와 관계 없이, 사물함 번호가 존재하고 회원 정보가 저장되어있을 시, 수정에 들어간다.
-	 * @param Lck1000VO
-	 *            lck1000VO
-	 * @return boolean
-	 * @throws Exception
-	 */
-	@Override
-	public void updateLck1000(Lck1000VO locker) throws Exception {
-		try {
-			String userId = locker.getUserId();
-			int lockerNum = locker.getLockerNum();
-			// 등록, 수정에서 공통으로 적용되는 검증 부분
-			if (commonValidate(userId, lockerNum, locker)) {
-				return;
-			}
-			// 이제 true의 상황 밖에 없으니깐 여기서는 등록을 한다.
-			lck1000DAO.saveLck1000(locker);
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
-
-	/**
-	 * Func : 사물함 삭제 메서드
-	 * 
-	 * @desc 사물함 삭제
-	 * @param int
-	 *            lockerNum
-	 * @return void
-	 * @throws Exception
-	 */
-	@Override
-	public void deleteLck1000(int lockerNum, Lck1000VO lck1000VO) throws Exception {
-		try {
-			if (!lck1000DAO.isContainsLck1000(lockerNum)) {
-				lck1000VO.setError("-1", message.getProperty("FAIL.DELETE"));
-				return;
-			}
-			lck1000DAO.deleteLck1000(lockerNum);
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-
-	}
-
-	/**
-	 * Func : 등록, 수정 시 공통으로 검증해야하는 내부 메서드
-	 * 
-	 * @desc 등록, 수정 시 공통으로 검증해야하는 부분 분리
-	 * @param String
-	 *            userId, int lockerNum
-	 * @return boolean
-	 * @throws Exception
-	 */
-	private boolean commonValidate(String userId, int lockerNum, Lck1000VO locker) throws Exception {
-		try {
-			// 회원 table에서 회원 id가 존재하는지 확인
-			if (!usr1000DAO.isContainsUsr1000(userId)) {
-				locker.setError("-1", message.getProperty("FAIL.CREATE.EXIT.MEMBER"));
-				return true;
-			}
-			// 사물함 테이블에서 사물함 번호가 존재하는지 확인
-			if (!lck1000DAO.isContainsLck1000(lockerNum)) {
-				locker.setError("-1", message.getProperty("FAIL.CREATE.EXIT.LOCKER"));
-				return true;
-			}
-			// 회원 정보를 가지고 온다.
-			Usr1000VO selectUsr = usr1000DAO.selectUsr1000(userId);
-
-			// 회원 id의 사용 유무가 n이 아닌지, 만료된 회원인지 검증
-			if (!Validator.isValidUser(selectUsr)) {
-				locker.setError("-1", message.getProperty("FAIL.CREATE.EXPIRE.MEMBER"));
-				return true;
-			}
-
-			// 회원 정보가 사물함 table에 존재
-			if (lck1000DAO.isContainsUsr1000(userId)) {
-				locker.setError("-1", message.getProperty("FAIL.CREATE.DUPLICATE.MEMBER"));
-				return true;
-			}
-			return false;
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-	}
+//
+//	/**
+//	 * Func : 회원 id 리스트 반환 메서드
+//	 * 
+//	 * @desc 회원 id 리스트 반환 메서드
+//	 * @param
+//	 * @return List<String>
+//	 * @throws Exception
+//	 */
+//	@Override
+//	public List<String> selectUsr1000List() throws Exception {
+//		try {
+//			List<Usr1000VO> selectUsr1000List = usr1000DAO.selectUsr1000List();
+//			List<String> userIds = new ArrayList<>();
+//			for (Usr1000VO user : selectUsr1000List) {
+//				userIds.add(user.getId());
+//			}
+//			return userIds;
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
+//
+//	/**
+//	 * Func : 사물함 등록 메서드
+//	 * 
+//	 * @desc 회원아이디가 존재하지 않고 비어있는 사물함 및 만료된 사물함에 한해서 등록 가능. 실패시 false 반환, 성공시 등록 후
+//	 *       true 반환
+//	 * @param Lck1000VO
+//	 *            lck1000VO
+//	 * @return boolean
+//	 * @throws Exception
+//	 */
+//	@Override
+//	public void createLck1000(Lck1000VO locker) throws Exception {
+//		String userId = locker.getUserId();
+//		try {
+//			int lockerNum = locker.getLockerNum();
+//
+//			// 등록, 수정에서 공통으로 적용되는 검증 부분
+//			if (commonValidate(userId, lockerNum, locker)) {
+//				return;
+//			}
+//
+//			// 넣으려는 사물함에 회원이 있고 만기일이 안넘으면 false
+//			Lck1000VO exsitLocker = lck1000DAO.selectLck1000(lockerNum);
+//
+//			// 해당 사물함에 회원이 존재하고 사물함 만료일이 지나지 않으면 false
+//			if (!exsitLocker.getUserId().isEmpty() && !Validator.isExpire(exsitLocker.getExpireDate())) {
+//				locker.setError("-1", message.getProperty("FAIL.CREATE.LOCKER"));
+//				return;
+//			}
+//
+//			// 이제 true의 상황 밖에 없으니깐 여기서는 등록을 한다.
+//			lck1000DAO.saveLck1000(locker);
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
+//
+//	/**
+//	 * Func : 사물함 수정 메서드
+//	 * 
+//	 * @desc 사물함에 회원의 존재 여부와 관계 없이, 사물함 번호가 존재하고 회원 정보가 저장되어있을 시, 수정에 들어간다.
+//	 * @param Lck1000VO
+//	 *            lck1000VO
+//	 * @return boolean
+//	 * @throws Exception
+//	 */
+//	@Override
+//	public void updateLck1000(Lck1000VO locker) throws Exception {
+//		try {
+//			String userId = locker.getUserId();
+//			int lockerNum = locker.getLockerNum();
+//			// 등록, 수정에서 공통으로 적용되는 검증 부분
+//			if (commonValidate(userId, lockerNum, locker)) {
+//				return;
+//			}
+//			// 이제 true의 상황 밖에 없으니깐 여기서는 등록을 한다.
+//			lck1000DAO.saveLck1000(locker);
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
+//
+//	/**
+//	 * Func : 사물함 삭제 메서드
+//	 * 
+//	 * @desc 사물함 삭제
+//	 * @param int
+//	 *            lockerNum
+//	 * @return void
+//	 * @throws Exception
+//	 */
+//	@Override
+//	public void deleteLck1000(int lockerNum, Lck1000VO lck1000VO) throws Exception {
+//		try {
+//			if (!lck1000DAO.isContainsLck1000(lockerNum)) {
+//				lck1000VO.setError("-1", message.getProperty("FAIL.DELETE"));
+//				return;
+//			}
+//			lck1000DAO.deleteLck1000(lockerNum);
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//
+//	}
+//
+//	/**
+//	 * Func : 등록, 수정 시 공통으로 검증해야하는 내부 메서드
+//	 * 
+//	 * @desc 등록, 수정 시 공통으로 검증해야하는 부분 분리
+//	 * @param String
+//	 *            userId, int lockerNum
+//	 * @return boolean
+//	 * @throws Exception
+//	 */
+//	private boolean commonValidate(String userId, int lockerNum, Lck1000VO locker) throws Exception {
+//		try {
+//			// 회원 table에서 회원 id가 존재하는지 확인
+//			if (!usr1000DAO.isContainsUsr1000(userId)) {
+//				locker.setError("-1", message.getProperty("FAIL.CREATE.EXIT.MEMBER"));
+//				return true;
+//			}
+//			// 사물함 테이블에서 사물함 번호가 존재하는지 확인
+//			if (!lck1000DAO.isContainsLck1000(lockerNum)) {
+//				locker.setError("-1", message.getProperty("FAIL.CREATE.EXIT.LOCKER"));
+//				return true;
+//			}
+//			// 회원 정보를 가지고 온다.
+//			Usr1000VO selectUsr = usr1000DAO.selectUsr1000(userId);
+//
+//			// 회원 id의 사용 유무가 n이 아닌지, 만료된 회원인지 검증
+//			if (!Validator.isValidUser(selectUsr)) {
+//				locker.setError("-1", message.getProperty("FAIL.CREATE.EXPIRE.MEMBER"));
+//				return true;
+//			}
+//
+//			// 회원 정보가 사물함 table에 존재
+//			if (lck1000DAO.isContainsUsr1000(userId)) {
+//				locker.setError("-1", message.getProperty("FAIL.CREATE.DUPLICATE.MEMBER"));
+//				return true;
+//			}
+//			return false;
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}
+//	}
 }
