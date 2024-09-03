5b02a37b2ebba6ec7dd2ac360ce654385e92bda4
taewan625 <taewan1997625@gmail.com>
2024-09-04 02:03:35 +0900

    Refactor : "사물함 db 및 사물함 VO 수정"
    ps. 직전 커밋의 lck1000.java는 사물함 목록 화면 - 해당 부분도 리팩토링 완료

--- src/lck/lck1000/lck1000/vo/Lck1000VO.java
+++ src/lck/lck1000/lck1000/vo/Lck1000VO.java
@@ -3,7 +3,6 @@ package lck.lck1000.lck1000.vo;
 import java.time.LocalDate;
 
 /**
- * @Class Name : Lck1000VO.java
  * @Description 사물함 관리 controller 리스트,등록,수정,삭제,조회 존재
  * @version 1.0
  * @author 권태완
@@ -12,84 +11,55 @@ import java.time.LocalDate;
  * @see Copyright (C) All right reserved.
  */
 public class Lck1000VO {
-	// 에러 코드
-	private String errCode = "1";
-	// 에러 문구
-	private String errMsg = "";
-
-	// 회원 번호
-	private String userId = "";
-	// 사물함 번호
+	//사물함 번호
 	private int lockerNum;
-	// 만료날짜
-	private LocalDate expireDate;
-
-	// 생성자
-	public Lck1000VO() {
-	}
-
-	public Lck1000VO(LocalDate exLocalDate) {
-		this.expireDate = exLocalDate;
-	}
-
-	public Lck1000VO(int lockerNum) {
-		this.lockerNum = lockerNum;
-	}
-
-	public Lck1000VO(String userId, int lockerNum) {
+	//회원 번호
+	private String userId;
+	//시작일자
+	private LocalDate startDate;
+	//만료일자
+	private LocalDate endDate;
+	
+	//생성자
+	public Lck1000VO() {};
+	
+	//생성자
+	public Lck1000VO(int lockerNum, String userId, LocalDate startDate, LocalDate endDate) {
 		super();
-		this.userId = userId;
 		this.lockerNum = lockerNum;
-	}
-
-	public Lck1000VO(String userId, int lockerNum, LocalDate expireDate) {
-		super();
 		this.userId = userId;
-		this.lockerNum = lockerNum;
-		this.expireDate = expireDate;
+		this.startDate = startDate;
+		this.endDate = endDate;
 	}
-
-	public Lck1000VO(String userId, int lockerNum, int expireDate) {
-		super();
-		this.userId = userId;
-		this.lockerNum = lockerNum;
-		this.expireDate = LocalDate.now().plusDays(expireDate);
-	}
-
-	// getter
-	public String getErrCode() {
-		return errCode;
-	}
-
-	public String getErrMsg() {
-		return errMsg;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
+	
+	/* getter & setter */
 	public int getLockerNum() {
 		return lockerNum;
 	}
-
-	public LocalDate getExpireDate() {
-		return expireDate;
+	public void setLockerNum(int lockerNum) {
+		this.lockerNum = lockerNum;
 	}
-
-	// setter
-	public void setError(String errCode, String errMsg) {
-		this.errCode = errCode;
-		this.errMsg = errMsg;
+	public String getUserId() {
+		return userId;
 	}
-
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
 	@Override
 	public String toString() {
-		String expireDate = "";
-		if (this.expireDate != null) {
-			expireDate = this.expireDate.toString();
-		}
-		return "사물함 상세 정보 [회원아이디=" + userId + ", 사물함 번호=" + lockerNum + ", 만료일=" + expireDate + "]";
+		return "Lck1000VO [lockerNum=" + lockerNum + ", userId=" + userId + ", startDate=" + startDate + ", endDate="
+				+ endDate + "]";
 	}
-
 }
